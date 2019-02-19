347a35edc7e2e92c332167474ab4d6e541ccc493
Oliver Lantwin <oliver.lantwin@cern.ch>
2019-02-19 14:33:04 +0100

    Blacken

--- canvasmagic.py
+++ canvasmagic.py
@@ -2,20 +2,25 @@
 # itself to be running already.  It only creates the magics subclass but
 # doesn't instantiate it yet.
 from __future__ import print_function
-from IPython.core.magic import (Magics, magics_class, line_magic,
-                                cell_magic, line_cell_magic)
-
-from IPython.core.magic_arguments import (
-    argument, magic_arguments, parse_argstring
+from IPython.core.magic import (
+    Magics,
+    magics_class,
+    line_magic,
+    cell_magic,
+    line_cell_magic,
 )
 
+from IPython.core.magic_arguments import argument, magic_arguments, parse_argstring
+
 import random
+
 
 def uniqueid():
     seed = random.getrandbits(32)
     while True:
-       yield seed
-       seed += 1
+        yield seed
+        seed += 1
+
 
 # The class MUST call this class decorator at creation time
 @magics_class
@@ -24,46 +29,64 @@ class CanvasMagics(Magics):
         # You must call the parent constructor
         super(CanvasMagics, self).__init__(shell)
         self.unique_sequence = uniqueid()
-        
+
     @magic_arguments()
     @argument(
-        '-s', '--size', action='store', type=str, default=None,
-        help='Pixel size of plots, "width,height". If not specified, use ROOT default.'
-        )
+        "-s",
+        "--size",
+        action="store",
+        type=str,
+        default=None,
+        help='Pixel size of plots, "width,height". If not specified, use ROOT default.',
+    )
     @argument(
-        '-S', '--save', action='store', type=str, default=None,
-        help='Save a copy to file, e.g., -S filename. Default is None'
-        )
+        "-S",
+        "--save",
+        action="store",
+        type=str,
+        default=None,
+        help="Save a copy to file, e.g., -S filename. Default is None",
+    )
     @argument(
-        '--name', action='store', type=str, default=None,
-        help='Set canvas name for further use.'
-        )
-    @argument(
-        'code',
-        nargs='*',
-        )
-    
+        "--name",
+        action="store",
+        type=str,
+        default=None,
+        help="Set canvas name for further use.",
+    )
+    @argument("code", nargs="*")
     @line_cell_magic
     def canvas(self, line, cell=None):
         args = parse_argstring(self.canvas, line)
         size = args.size
-        width, height = size.split(',') if size else (None, None)
+        width, height = size.split(",") if size else (None, None)
         uid = next(self.unique_sequence)
         if not cell:
-            cell = str('').join(args.code)
-        constructor = "ROOT.TCanvas('{uid}', '{uid}', {width}, {height})".format(
-            uid=uid,
-            width=width,
-            height=height
-        ) if size else "ROOT.TCanvas()"
-        canvas_name = args.name if args.name else '__c_{uid}__'.format(uid=uid)
-        cell = canvas_name + " = {constructor}\n".format(constructor=constructor) + cell + "\n" + canvas_name + ".Draw()"
+            cell = str("").join(args.code)
+        constructor = (
+            "ROOT.TCanvas('{uid}', '{uid}', {width}, {height})".format(
+                uid=uid, width=width, height=height
+            )
+            if size
+            else "ROOT.TCanvas()"
+        )
+        canvas_name = args.name if args.name else "__c_{uid}__".format(uid=uid)
+        cell = (
+            canvas_name
+            + " = {constructor}\n".format(constructor=constructor)
+            + cell
+            + "\n"
+            + canvas_name
+            + ".Draw()"
+        )
         if args.save:
             cell += "\n" + canvas_name + ".SaveAs({})".format(args.save)
         self.shell.run_cell(cell)
 
+
 # In order to actually use these magics, you must register them with a
 # running IPython.
+
 
 def load_ipython_extension(ipython):
     """
